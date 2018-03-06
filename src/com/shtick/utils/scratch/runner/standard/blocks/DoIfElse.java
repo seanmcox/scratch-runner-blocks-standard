722167909921cfb3d88d28b6b7160f64abb84ef6
seanmcox <github@smcox.com>
2018-03-06 00:01:07 -0700

    Updated to handle null code blocks in doifelse.

--- src/com/shtick/utils/scratch/runner/standard/blocks/DoIfElse.java
+++ src/com/shtick/utils/scratch/runner/standard/blocks/DoIfElse.java
@@ -8,6 +8,7 @@ import com.shtick.utils.scratch.runner.core.elements.BlockTuple;
 import com.shtick.utils.scratch.runner.core.elements.control.BasicJumpBlockTuple;
 import com.shtick.utils.scratch.runner.core.elements.control.FalseJumpBlockTuple;
 import com.shtick.utils.scratch.runner.core.elements.control.TestBlockTuple;
+import com.shtick.utils.scratch.runner.core.elements.control.TrueJumpBlockTuple;
 
 /**
  * @author sean.cox
@@ -38,7 +39,32 @@ public class DoIfElse implements OpcodeControl{
 	public BlockTuple[] execute(java.util.List<Object> arguments) {
 		java.util.List<BlockTuple> subtuples1 = (java.util.List<BlockTuple>)arguments.get(1);
 		java.util.List<BlockTuple> subtuples2 = (java.util.List<BlockTuple>)arguments.get(2);
-		BlockTuple[] retval = new BlockTuple[subtuples1.size()+subtuples2.size()+3];
+		BlockTuple[] retval;
+		if(subtuples1==null) {
+			if(subtuples2==null)
+				return new BlockTuple[0];
+			retval = new BlockTuple[subtuples2.size()+2];
+			retval[0] = new TestBlockTuple(arguments.get(0));
+			retval[1] = new TrueJumpBlockTuple(retval.length);
+			int i=2;
+			for(BlockTuple subtuple:subtuples2) {
+				retval[i] = subtuple;
+				i++;
+			}
+			return retval;
+		}
+		if(subtuples2==null) {
+			retval = new BlockTuple[subtuples1.size()+2];
+			retval[0] = new TestBlockTuple(arguments.get(0));
+			retval[1] = new FalseJumpBlockTuple(retval.length);
+			int i=2;
+			for(BlockTuple subtuple:subtuples1) {
+				retval[i] = subtuple;
+				i++;
+			}
+			return retval;
+		}
+		retval = new BlockTuple[subtuples1.size()+subtuples2.size()+3];
 		retval[0] = new TestBlockTuple(arguments.get(0));
 		retval[1] = new FalseJumpBlockTuple(subtuples1.size()+3);
 		int i=2;
@@ -54,30 +80,4 @@ public class DoIfElse implements OpcodeControl{
 		}
 		return retval;
 	}
-
-//	/* (non-Javadoc)
-//	 * @see com.shtick.utils.scratch.runner.core.OpcodeAction#execute(com.shtick.utils.scratch.runner.core.ScratchRuntime, com.shtick.utils.scratch.runner.core.ScriptTupleRunner, com.shtick.utils.scratch.runner.core.elements.ScriptContext, java.lang.Object[])
-//	 */
-//	@Override
-//	public void execute(ScratchRuntime runtime, ScriptTupleRunner scriptRunner, ScriptContext context,
-//			Object[] arguments) {
-//		if(arguments.length!=3)
-//			throw new IllegalArgumentException("3 arguments expected for "+getOpcode());
-//		if(!OpcodeUtils.isEvaluable(arguments[0]))
-//			throw new IllegalArgumentException("The first argument for opcode, "+getOpcode()+", must be evaluable.");
-//		if((!(arguments[1] instanceof java.util.List<?>))&&(arguments[1]!=null))
-//			throw new IllegalArgumentException("List of BlockTuples expected, but "+arguments[0].getClass().getCanonicalName()+" found.");
-//		if((!(arguments[2] instanceof java.util.List<?>))&&(arguments[2]!=null))
-//			throw new IllegalArgumentException("List of BlockTuples expected, but "+arguments[0].getClass().getCanonicalName()+" found.");
-//		Object value = arguments[0];
-//		if(!(value instanceof Boolean))
-//			throw new IllegalArgumentException("Boolean condition expected.\nFound: "+value+"\nEvaluated from: "+arguments[0]+"-"+((arguments[0] instanceof BlockTuple)?((BlockTuple)arguments[0]).getOpcode():""));
-//		if(((Boolean)value).booleanValue()) {
-//			scriptRunner.runBlockTuples(context, (java.util.List<BlockTuple>)arguments[1]);
-//		}
-//		else {
-//			scriptRunner.runBlockTuples(context, (java.util.List<BlockTuple>)arguments[2]);
-//		}
-//	}
-
 }
