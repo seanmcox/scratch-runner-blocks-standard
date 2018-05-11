b7622320fd66e4265a569e68ebe25dfa251390dd
Sean M. Cox <github@smcox.com>
2018-05-11 12:32:22 -0600

    Added DoAsk button to UI.

--- src/com/shtick/utils/scratch/runner/standard/blocks/aux/DoAskUI.java
+++ src/com/shtick/utils/scratch/runner/standard/blocks/aux/DoAskUI.java
@@ -14,10 +14,17 @@ import java.awt.Graphics2D;
 import java.awt.Rectangle;
 import java.awt.Shape;
 import java.awt.Stroke;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.geom.RoundRectangle2D;
 
+import javax.swing.AbstractButton;
 import javax.swing.BorderFactory;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
@@ -47,6 +54,8 @@ public class DoAskUI extends JPanel{
 	private static FontMetrics FONT_METRICS_LARGE;
 	
 	private JTextField textField = new JTextField();
+	// TODO Stretch button image for unusual stage sizes.
+	private DoAskButton button = new DoAskButton();
 	
 	private int labelWidth;
 	private int fontHeight;
@@ -64,7 +73,8 @@ public class DoAskUI extends JPanel{
 		label.setFont(FONT_NORMAL);
 		label.setForeground(Color.BLACK);
 		add(label, BorderLayout.NORTH);
-		add(textField,BorderLayout.SOUTH);
+		add(textField,BorderLayout.CENTER);
+		add(button,BorderLayout.EAST);
 		textField.setBackground(new Color(242,242,242));
 		textField.setBorder(BorderFactory.createMatteBorder(3, 3, 0, 0, Color.WHITE));
 		setDoubleBuffered(false);
@@ -129,6 +139,7 @@ public class DoAskUI extends JPanel{
 	 */
 	public void addActionListener(ActionListener actionListener) {
 		textField.addActionListener(actionListener);
+		button.addActionListener(actionListener);
 	}
 
 	/**
@@ -137,6 +148,7 @@ public class DoAskUI extends JPanel{
 	 */
 	public void removeActionListener(ActionListener actionListener) {
 		textField.removeActionListener(actionListener);
+		button.removeActionListener(actionListener);
 	}
 	
 	/**
@@ -154,5 +166,58 @@ public class DoAskUI extends JPanel{
 	public void reshape(int x, int y, int w, int h) {
 		super.reshape(x, y, w, h);
 		doLayout();
+	}
+	
+	private class DoAskButton extends AbstractButton{
+		private ImageIcon image = new ImageIcon(getClass().getResource("/com/shtick/utils/scratch/runner/standard/blocks/aux/DoAskButton.png"));
+		
+		public DoAskButton() {
+			addMouseListener(new MouseListener() {
+				@Override
+				public void mouseReleased(MouseEvent e) {}
+				
+				@Override
+				public void mousePressed(MouseEvent e) {}
+				
+				@Override
+				public void mouseExited(MouseEvent e) {}
+				
+				@Override
+				public void mouseEntered(MouseEvent e) {}
+				
+				@Override
+				public void mouseClicked(MouseEvent e) {
+					if(e.getClickCount()==1) {
+						DoAskButton.this.fireActionPerformed(new ActionEvent(DoAskUI.this, ActionEvent.ACTION_FIRST, ""));
+					}
+				}
+			});
+		}
+
+		/* (non-Javadoc)
+		 * @see javax.swing.AbstractButton#getIcon()
+		 */
+		@Override
+		public Icon getIcon() {
+			return image;
+		}
+
+		/* (non-Javadoc)
+		 * @see javax.swing.JComponent#getPreferredSize()
+		 */
+		@Override
+		public Dimension getPreferredSize() {
+			return new Dimension(image.getIconWidth()/2, image.getIconHeight()/2);
+		}
+
+		/* (non-Javadoc)
+		 * @see javax.swing.JComponent#paint(java.awt.Graphics)
+		 */
+		@Override
+		public void paint(Graphics g) {
+			Graphics2D g2 = (Graphics2D)g;
+			g2.scale(0.5, 0.5);
+			g2.drawImage(image.getImage(), 0, 0, null);
+		}
 	}
 }
